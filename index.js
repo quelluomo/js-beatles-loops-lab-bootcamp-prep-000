e250f154e07c975b9bd6dbcbdc315c44a256dd14
Matteo Muscas <thatman.j@gmail.com>
2018-09-17 10:18:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(){
         "He hated the sound of his own voice"
         ]
         while (john > 0){
-          return(john);
-          
+          console.log(john);
+          return '!!!'
         }
 }
