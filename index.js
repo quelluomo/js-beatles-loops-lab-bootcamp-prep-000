3c2136e60605cea343fcbcfe17052329ac845540
Matteo Muscas <thatman.j@gmail.com>
2018-09-17 10:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
     return array;
 }
 function johnLennonFacts(array){
-  const facts = ["He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-        ]
+  const facts = []
         while (i > 0){
           facts.push(i + '!!!');
           return newFact;
