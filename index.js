86f9778fb92609fae735d48b7ba75a857f641f45
Matteo Muscas <thatman.j@gmail.com>
2018-09-17 12:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function johnLennonFacts(array){
         return facts;
         }
 }
+
+function johnLennonFacts (facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+    }
+  return array;
+}
