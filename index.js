0769530f3fde8cd6517bdee69b08e50b7fe200de
Matteo Muscas <thatman.j@gmail.com>
2018-09-17 08:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+  
+  var array = [];
+  
+    for (let i = 0; musicians.length && instruments.length; i++){
+      array.push(`${musicians} plays ${instruments}`);
+    }
+    
+    return array;
+}
+
+/*
+for (var i = 1; i < 100; i++) {
+  console.log("Hello World the " + i + " time");
+}
+*/
